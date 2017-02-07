ae1d6f8b9bedb495243f06b71ee280750b6339f0
zhengwei931102 <admin@admindemac-mini.local>
2017-02-07 15:40:52 +0800

    Initial Commit

---
+++ titlebar/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  titlebar
+//
+//  Created by admin on 17/2/7.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
